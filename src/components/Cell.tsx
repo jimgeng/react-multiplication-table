ec0919f71726bbb14bef84b28eb908b6e920204e
Jim Geng <jimgeng2004@gmail.com>
2023-09-14 18:41:06 -0700

    last minute css hotfix

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -21,7 +21,7 @@ export default function Cell({ children, header = false, row, col, clickFn }: Ce
   }
   
   return (
-    <div onClick={handleClick} className={`flex justify-center items-center h-12 w-12 flex-shrink-0 bg-gray-200 hover:bg-slate-300 dark:bg-gray-800 dark:hover:bg-gray-900 transition-colors ${header ? "cursor-pointer" : ""}`}>
+    <div onClick={handleClick} className={`flex justify-center items-center h-12 w-12 flex-shrink-0 bg-gray-200 dark:bg-gray-800  transition-colors ${header ? "" : "dark:hover:bg-gray-900 hover:bg-slate-300 cursor-pointer"}`}>
       <p className={`${header ? "text-emerald-500" : "dark:text-gray-200" } text-xl font-bold`}>{children}</p>
     </div>
   );
